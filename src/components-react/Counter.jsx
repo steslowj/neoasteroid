42bb8926abd9d81c6d5f2898f4eb22ac77e4eb4b
steslowj <steslowj@gmail.com>
2025-08-28 16:58:11 -0700

    Initial commit

---
+++ src/components-react/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="flex gap-4 justify-center items-center">
+      <p>ʕ ·(エ)· ʔ</p>
+      <p>clicky</p>
+      <button className="btn btn-accent" type="button" onClick={(e)=>setCount(count+1)}>{count}</button>
+    </div>
+  )
+}
+export default Counter;
