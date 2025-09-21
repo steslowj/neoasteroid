0059b990c79292d7ba69163f197a614bc318ea7d
Jessica Steslow <124192196+steslowj@users.noreply.github.com>
2025-09-20 19:43:53 -0700

    styles, DS code draft

--- src/components-react/Counter.jsx
+++ src/components-react/Counter.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="flex gap-4 justify-center items-center">
-      <p>ʕ ·(エ)· ʔ</p>
+      <p class="text-nowrap">ʕ ·(エ)· ʔ</p>
       <p>clicky</p>
-      <button className="btn btn-accent" type="button" onClick={(e)=>setCount(count+1)}>{count}</button>
+      <button
+        className="btn btn-accent"
+        type="button"
+        onClick={(e) => setCount(count + 1)}
+      >
+        {count}
+      </button>
     </div>
-  )
-}
+  );
+};
 export default Counter;
